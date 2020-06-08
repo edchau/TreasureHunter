4e9261edf5b0e24567c10eed6672d49312d823b0
Edward Chau <edwardchau@berkeley.edu>
2020-06-07 23:54:45 -0700

    movement

---
+++ src/features/map/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { SPRITE_SIZE } from '../../config/constants'
+import './styles.css'
+
+const getTileSprite = type => {
+    switch(type) {
+        case 0:
+            return 'grass'
+        case 3:
+            return 'tree'
+        case 4:
+            return 'chest'
+        case 5:
+            return 'rock'
+        case 6:
+            return 'tree'
+    }
+}
+
+const MapTile = props => {
+    return <div
+        className={`tile ${getTileSprite(props.tile)}`}
+        style={{
+            height: SPRITE_SIZE,
+            width: SPRITE_SIZE
+        }}/>
+}
+
+const MapRow = props => {
+    return <div className="row">
+            { props.tiles.map( tile => <MapTile tile={tile}/> ) }
+        </div>
+}
+
+const Map = props => {
+    return (<div
+            style={{
+                width: '800px',
+                height: '480px',
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                border: '4px solid white'
+            }}
+        >
+                { props.tiles.map( row => <MapRow tiles={row} /> ) }
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        tiles: state.map.tiles
+    }
+}
+
+export default connect(mapStateToProps)(Map)
